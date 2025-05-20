6fd7488a92d63e8a13784ec0f5669111dfc7b2cc
daguo1885 <99662899+daguo1885@users.noreply.github.com>
2025-05-20 10:26:04 +0800

    Add files via upload

--- tiku_app.py
+++ tiku_app.py
@@ -13,15 +13,16 @@ import random
 from io import BytesIO
 from streamlit import cache_data
 import streamlit.components.v1 as components
+import sqlite3
 warnings.simplefilter("ignore")
 CURRENT_PATH = os.path.dirname(__file__)
-pic = r'icon2.png'
+pic = r'static\icon2.png'
 
 
 # 初始化设置：streamlit页面基本配置
 def init_set(hide=False):
     st.set_page_config(
-        page_title="技能竞赛理论题测试系统",
+        page_title="应急救护竞赛理论题测试系统",
         page_icon=":100:",
         layout="wide",  # wide, centered
         initial_sidebar_state="expanded"   # 侧边栏 auto
@@ -52,10 +53,8 @@ def init_set(hide=False):
             padding-left: 1rem; padding-right: 1rem; padding-top: 1rem;}                                    
         </style>""", unsafe_allow_html=True)
     # 全局变量设置
-    if 'first_visit' not in st.session_state:
-        st.session_state.first_visit = True
-    else:
-        st.session_state.first_visit = False
+    if 'role' not in st.session_state:
+        st.session_state.role = ""
 
     if 'index' not in st.session_state:
         st.session_state.index = True
@@ -71,6 +70,9 @@ def init_set(hide=False):
 def side_bar(bar):
     bar.empty()
     with bar.container():
+        if st.session_state.role != '':
+            st.write(f':blue[**{st.session_state.role}**]，欢迎你！')
+
         st.markdown("<BR/><font size=4 color=darkblue>**试题数量选择**</font>", unsafe_allow_html=True)
         df = get_tiku().fillna('')
         dfa = df.loc[df['试题类型'] == '单选'].reset_index(drop=True)
@@ -79,7 +81,8 @@ def side_bar(bar):
         # with st.sidebar.form("试题选择"):
         dx = st.slider('单选题数量', 0, 50, 10)
         pd = st.slider('判断题数量', 0, 50, 10)
-        mx = st.slider('多选题数量', 0, 50, 10)
+        # mx = st.slider('多选题数量', 0, 50, 10)
+        mx = 0
        # sd_btn = st.form_submit_button('生成试卷')
         sd_btn = st.button('刷新试卷')
 
@@ -108,13 +111,24 @@ def menu_bar():
         st.session_state.sel_menu = '在线测试'
 
     st.session_state.sid = st.sidebar.empty()
+
+    if st.session_state.role == "":
+        with st.session_state.sid:
+            st.session_state.role = st.text_input('请输入员工号记录你的成绩')
+    else:
+        with st.session_state.sid:
+            st.write(f':blue[**{st.session_state.role}**]欢迎你！')
+
     st.sidebar.divider()
     st.sidebar.caption(':copyright: 2024 ChongQingCabin by Daguo')
 
 
 @cache_data
 def get_tiku():
-    df = pd.read_excel('data.xlsx')
+    cox = sqlite3.connect('data.db')
+    # df = pd.read_excel('data.xlsx').fillna('')
+    # df.to_sql('data', cox, if_exists='replace')
+    df = pd.read_sql("select * from data", cox)
     return df
 
 
@@ -131,7 +145,7 @@ def main_app():
         # 生成单选题
         st.markdown("<br/>", unsafe_allow_html=True)
         st.markdown('<font size=4 color=darkblue>**一. 单项选择题**</font>', unsafe_allow_html=True)
-        dx_tbl = pd.DataFrame(None, columns=['题目', '你的选择', '正确答案'])
+        dx_tbl = pd.DataFrame(None, columns=['序号','题目', '你的选择', '正确答案'])
         for i, j in enumerate(st.session_state['danxuan']):
             _sel = []
             _sel_a = ''
@@ -144,7 +158,7 @@ def main_app():
             xuz = st.radio(":blue" + _tigan, _sel, None, horizontal=True)
             if xuz is not None:
                 xuz = xuz[0]
-            df = pd.DataFrame([[_tigan + "\n" + _sel_a, xuz, dfa['答案'][j]]], columns=['题目', '你的选择', '正确答案'])
+            df = pd.DataFrame([[dfa['序号'][j], _tigan + "\n" + _sel_a, xuz, dfa['答案'][j]]], columns=['序号', '题目', '你的选择', '正确答案'])
             dx_tbl = pd.concat([dx_tbl, df])
 
         # 生成判断题
@@ -154,30 +168,31 @@ def main_app():
             _sel_1 = []
             _tigan_1 = f"[判断第{i+1}题]  {dfb['试题题干'][j]}"
             xuz_1 = st.radio(":blue" + _tigan_1, ['正确', '错误'], None, horizontal=True)
-            df1 = pd.DataFrame([[_tigan_1, xuz_1, dfb['答案'][j]]], columns=['题目', '你的选择', '正确答案'])
+            df1 = pd.DataFrame([[dfb['序号'][j], _tigan_1, xuz_1, dfb['答案'][j]]], columns=['序号', '题目', '你的选择', '正确答案'])
             dx_tbl = pd.concat([dx_tbl, df1])
 
         # 生成多选题
-        st.markdown('<font size=4 color=darkblue>**三. 多项选择题**</font>', unsafe_allow_html=True)
-        for i, j in enumerate(st.session_state['duoxuan']):
-            st.markdown("<br/>", unsafe_allow_html=True)
-            _tigan_2 = f"[多选第{i+1}题]  {dfc['试题题干'][j]}"
-            st.write(":blue" + _tigan_2)
-            _sel_2 = []
-            _sel_2_a = ''
-            xuz_2 = ''
-            for z in ['A', 'B', 'C', 'D', 'E']:
-                if dfc[z][j] !="":
-                    _sel_2.append(z + '. ' + str(dfc[z][j]))
-                    _sel_2_a = _sel_2_a + '\n' + z + '. ' + str(dfc[z][j])
-                    ans_a = st.checkbox(z + '. ' + str(dfc[z][j]))
-                    if ans_a:
-                        xuz_2 += z
+        if len(st.session_state['duoxuan']) > 0:
+            st.markdown('<font size=4 color=darkblue>**三. 多项选择题**</font>', unsafe_allow_html=True)
+            for i, j in enumerate(st.session_state['duoxuan']):
+                st.markdown("<br/>", unsafe_allow_html=True)
+                _tigan_2 = f"[多选第{i+1}题]  {dfc['试题题干'][j]}"
+                st.write(":blue" + _tigan_2)
+                _sel_2 = []
+                _sel_2_a = ''
+                xuz_2 = ''
+                for z in ['A', 'B', 'C', 'D', 'E']:
+                    if dfc[z][j] !="":
+                        _sel_2.append(z + '. ' + str(dfc[z][j]))
+                        _sel_2_a = _sel_2_a + '\n' + z + '. ' + str(dfc[z][j])
+                        ans_a = st.checkbox(z + '. ' + str(dfc[z][j]))
+                        if ans_a:
+                            xuz_2 += z
 
-            if xuz_2 is not None:
-                xuz_2 = [''.join(n[0]) for n in xuz_2]
-            df2 = pd.DataFrame([[_tigan_2 + "\n" + _sel_2_a, ''.join(xuz_2), dfc['答案'][j]]], columns=['题目', '你的选择', '正确答案'])
-            dx_tbl = pd.concat([dx_tbl, df2])
+                if xuz_2 is not None:
+                    xuz_2 = [''.join(n[0]) for n in xuz_2]
+                df2 = pd.DataFrame([[dfc['序号'][j], _tigan_2 + "\n" + _sel_2_a, ''.join(xuz_2), dfc['答案'][j]]], columns=['序号', '题目', '你的选择', '正确答案'])
+                dx_tbl = pd.concat([dx_tbl, df2])
 
         st.session_state['dx_tbl'] = dx_tbl
 
@@ -189,7 +204,7 @@ def main_app():
 
 
 def result_show():
-    st.markdown('<font size=4 color=darkblue>**练习测试结果**</font>', unsafe_allow_html=True)
+    st.markdown(f'<font size=4 color=darkblue>**{st.session_state.role}练习测试结果**</font>', unsafe_allow_html=True)
     timu_num = st.session_state['dx_tbl'].shape[0]
     ok_num = sum(st.session_state.dx_tbl['你的选择'] == st.session_state.dx_tbl['正确答案'])
     rat = ok_num / timu_num
@@ -209,10 +224,17 @@ def result_show():
     show_w = st.toggle('只显示答错题目', on_change=stat_change)
     save_excel(st.session_state['dx_tbl'], '练习题', label='下载全部试卷')
     save_excel(df_wrong, '错题单', label='下载答错题目')
+    # 此处保存答题数据
+    dfa = st.session_state['dx_tbl']
+    dfa['用户'] = st.session_state.role
+    dfa['时间'] = datetime.now()
+    cox = sqlite3.connect('data.db')
+    dfa.to_sql('测试记录', cox, if_exists='replace')
+    # st.dataframe(dfa)
     if show_w:
-        show_table(df_wrong)
+        show_table(df_wrong[['题目', '你的选择', '正确答案']])
     else:
-        show_table(st.session_state.dx_tbl)
+        show_table(st.session_state.dx_tbl[['题目', '你的选择', '正确答案']])
 
 
 def stat_change():
@@ -220,11 +242,23 @@ def stat_change():
 
 
 def index_page():
-    st.subheader(':rainbow[技能竞赛理论题测试系统]', divider='rainbow')
-    st.write('技能竞赛理论试题在线刷题工具，系统根据官方下发理论题库制作，集成题库学习和在线测试功能。')
+    st.subheader(':rainbow[应急救护竞赛理论题测试系统]', divider='rainbow')
+    st.write('👈️**请在侧边菜单栏选择相应功能**')
+    st.write('竞赛理论试题在线刷题工具，系统根据官方下发理论题库制作，集成题库学习和在线测试功能。')
     st.write('在线学习可以自主选择:blue[**顺序复习、随机复习、批量阅读**]的方式。')
     st.write('在线测试可以自定义生成试卷时各种题型的数量。')
-    st.write('👈️请在侧边菜单栏选择相应功能')
+    st.write(':blue[提示：]完成测试后下载错误试题为excel格式，可手工将题干列设置为自动换行并调整列宽，可获得较好的显示效果。')
+    st.markdown('</br>', unsafe_allow_html=True)
+    st.write(":green[以下方式可以展开或隐藏侧边栏菜单]")
+
+    if st.session_state.role != '':
+        st.session_state.sid.write(f':blue[**{st.session_state.role}**]，欢迎你！')
+
+    cl = st.columns(4)
+    with cl[0]:
+        st.image("static/zhankai.jpg", width=200)
+    with cl[1]:
+        st.image("static/zhedie.jpg", width=200)
 
 
 def show_table(df, **kwargs):
@@ -260,6 +294,7 @@ def trans_record_data_to_io(data, **kwargs):
     return file.getvalue()
 
 
+@st.fragment
 def save_excel(df, file_name, *args, **kwargs):
     try:
         fn = f'{file_name}{args[0]}.xlsx'
